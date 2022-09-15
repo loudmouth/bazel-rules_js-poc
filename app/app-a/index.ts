5d0c97439e246be278dbdfd0f301dfb41a905f3b
JP Wright <jp@gojust.com>
2022-09-15 11:16:51 +0200

    import lib-data into app-a; not runnable yet

--- app/app-a/index.ts
+++ app/app-a/index.ts
@@ -1,7 +1,12 @@
 import pino from "pino";
 import lib from "just/lib/lib-a";
+import data from "just/lib/lib-data";
 
 const logger = pino();
 logger.info("testing 1, 2, 3");
 
 logger.info(lib());
+// people();
+// others();
+
+console.log(data);
---
+++ lib/lib-data/index.ts
@@ -0,0 +1,8 @@
+import data from "./data.json";
+export const people = () => {
+  return data.people;
+};
+export const others = () => {
+  return data.others;
+};
+export default { data };
